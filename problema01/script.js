6654f0aae8480093123ecb85ece4b4b0b0a06cc1
Ricardo Gambini <ricardo.gambini18@outlook.com>
2020-09-26 09:31:26 -0500

    Subiendo problema 01

---
+++ problema01/script.js
@@ -0,0 +1,25 @@
+//boton
+const boton = document.getElementById("boton");
+boton.addEventListener("click", sumarArgumentos);
+
+//Texto
+const paso02 =document.getElementById("paso02");
+
+function sumarArgumentos(){
+    //Reciviendo datos
+    let argumento1 = parseInt(document.getElementById("argumento1").value);
+    let argumento2 = parseInt(document.getElementById("argumento2").value);
+    
+    //Validando datos
+    if((argumento1)&&(argumento2)){
+        //Solucion
+        let resultado = argumento1 + argumento2;
+
+        //Escribiendo
+        paso02.innerHTML = `${argumento1} + ${argumento2} = ${resultado}`;
+    }
+    else{
+        paso02.innerHTML = "Ingrese bien los datos."
+    }
+}
+    
